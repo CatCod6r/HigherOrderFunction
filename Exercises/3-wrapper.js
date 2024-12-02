4f0333593e1bf0bc0f181cfdd36f8689ad2df765
CatCod6r <165474293+CatCod6r@users.noreply.github.com>
2024-12-02 14:18:59 +0000

    Update 3-wrapper.js

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+onst contract = (fn, ...typeDefs) => (...parameters) => {
+  for (let i = 0; i < parameters.length; i++) {
+    const currentParam = parameters[i];
+    const expectedType = typeDefs[i];
+    const typeName = expectedType.name.toLowerCase();
+    if (typeof currentParam !== typeName) {
+      throw new TypeError(`Argument type ${typeName} expected`);
+    }
+  }
+  const result = fn(...parameters);
+  const resultTypeDef = typeDefs[typeDefs.length - 1];
+  const resultTypeName = resultTypeDef.name.toLowerCase();
+  if (typeof result !== resultTypeName) {
+    throw new TypeError(`Result type ${resultTypeName} expected`);
+  }
+  return result;
+};
 
 module.exports = { contract };
