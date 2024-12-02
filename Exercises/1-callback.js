5d4a25697895a9cb2ff7fcd3f4fa5a060c952373
CatCod6r <165474293+CatCod6r@users.noreply.github.com>
2024-12-02 14:16:44 +0000

    Update 1-callback.js

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (collection, handler) => {
+  const propertyNames = Object.keys(collection);
+  for (const property of propertyNames) {
+    const propertyValue = collection[property];
+    handler(property, propertyValue, collection);
+  }
+};
 
 module.exports = { iterate };
